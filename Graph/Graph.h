068e8c97c5501db7600510bba80e57b1442236b5
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-05-07 20:01:58 +0200

    Initial Commit

---
+++ Graph/Graph.h
@@ -0,0 +1,19 @@
+//
+//  Graph.h
+//  Graph
+//
+//  Created by Carlos Rodríguez Domínguez on 7/5/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graph.
+FOUNDATION_EXPORT double GraphVersionNumber;
+
+//! Project version string for Graph.
+FOUNDATION_EXPORT const unsigned char GraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graph/PublicHeader.h>
+
+
